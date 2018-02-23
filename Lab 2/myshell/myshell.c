f12a08ed24d01855b237179c4e34d3bd0d4190df
Abdi Ibrahim <abdirisak.ibrahim@uoit.net>
2018-02-23 14:11:28 -0500

    Moved commands to utility.c, added I/O redirection

--- Lab 2/myshell/myshell.c
+++ Lab 2/myshell/myshell.c
@@ -10,17 +10,16 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <string.h>
 #include "utility.h"
 #include "myshell.h"
-#include <unistd.h>
-//#include <curses.h> need to install
 
 // Put macros or constants here using #define
 #define BUFFER_LEN 256
 
 // Put global environment variables here
-extern char **environ;
+char args[10][256];
 
 // Define functions declared in myshell.h here
 
@@ -30,120 +29,48 @@ int main(int argc, char *argv[])
     char buffer[BUFFER_LEN] = { 0 };
     char command[BUFFER_LEN] = { 0 };
     char arg[BUFFER_LEN] = { 0 };
-    unsigned long i;
-    char path[1024];
-    // Parse the commands provided using argc and argv
-    if (getcwd(path, sizeof(path)) != NULL){
-	char com[1024]= {"shell="};
-	char str[2048]= {};
-	char myshell[1024] ={"/myshell"};	
-	strcat(str,com);
-	strcat(str,path);
-	strcat(str,myshell);
- 	putenv(str);
+    int errorCode;
+
+	// Parse the commands provided using argc and argv
+    int i;
+	char *myshell = getenv("PWD");
+	setenv("PWD", myshell, 2);
+	strcat(myshell, "/myshell");
+	setenv("SHELL", myshell, 1);
+
+	for (i = 0; i < argc; ++i){
+		printf("\n");
+		strcpy(command, argv[i]);
 	}
+	
     // Perform an infinite loop getting command input from users
-	char cwd[1024];
-	getcwd(cwd, sizeof(cwd));
-       	//fprintf(stdout, "%s\n", cwd);
-	printf("%s:$ ", cwd);
-    while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
-    {
-	char cwd[1024];
-	getcwd(cwd, sizeof(cwd));
-       	//fprintf(stdout, "%s\n", cwd);
-	printf("%s\n", cwd);
+    while (printf("%s: ", getenv("PWD")), fgets(buffer, BUFFER_LEN, stdin) != NULL){
         // Perform string tokenization to get the command and argument
-	char* token = strtok(buffer, " \n");
-	for (i=0; i < sizeof(buffer);i++){
-		command[i] = token[i];
-	}
-	token = strtok(NULL," \n");
-	if (token != NULL){
-		for (i=0; i < sizeof(buffer);i++){
-		arg[i] = token[i];
-		}
-	}
-	printf("command = %s\n",command);
-	printf("arg = %s\n",arg);
-	
-        // Check the command and execute the operations for each command
-        // cd command -- change the current directory
-        if (strcmp(command, "cd") == 0)
-        {
-		if(strcmp(arg, "") == 0){
-			if (getcwd(cwd, sizeof(cwd)) != NULL)
-       				fprintf(stdout, "Current working dir: %s\n", cwd);
-				
-		}
-		
-		else{
-			int result = 0;
-			result = chdir(arg);
-  			if(result == 0){
-    				if (getcwd(cwd, sizeof(cwd)) != NULL)
-       					fprintf(stdout, "Current working dir: %s\n", cwd);
+		strcpy(arg, buffer);
+		int numTok = 0;
+		char *token = NULL;
 
+		for (token = strtok(buffer, " \n\0\r"); NULL != token; token = strtok(NULL, " \n\0\r")){
+			if (numTok == 0){
+				strcpy(command, token);
+				printf("Command:%s\n", command);
 			}
-  
-  			else{
-    				 printf("Couldn't change directory to %s\n", arg ); 
-    			}
-  		}	
-        }
-	else if (strcmp(command,"clr") == 0){
-		/*initscr(); 
-		clear();
-		refresh();
-		endwin();*/
-		//above not working proper and uses curses
-		int n;
-    		for (n = 0; n < 10; n++)
-      			printf( "\n\n\n\n\n\n\n\n\n\n" );
-	}
-	else if (strcmp(command,"environ") == 0){
-		int i = 1;
-  		char *s = *environ;
-  		for (; s; i++) {
-    			printf("%s\n", s);
-    			s = *(environ+i);
-  		}
-	}
-	else if (strcmp(command,"echo") == 0){
-		char* string = strtok(NULL, "\n");
-		printf("%s %s\n",arg,string);
-	}
-	
-	else if (strcmp(command,"help") == 0)
-	{
-		int c;
-		FILE *file;
-		file = fopen("README","r");
-		if (file){
-			while((c=getc(file)) !=EOF)
-				putchar(c);
-			fclose(file);		
+			else{
+				strcpy(args[numTok], token);
+				printf("Arguement:%s\n", args[numTok]);
+			}
+			++numTok;
+		}
+
+		// Send commands to be executed
+		errorCode = commands(command, args, numTok);
+		if (errorCode == 1){
+			break;
+		}
+		if (errorCode == -1){
+			printf("Error");
+			exit(1);
 		}
 	}
-
-        // other commands here...
-        
-        // quit command -- exit the shell
-        else if (strcmp(command, "quit") == 0)
-        {
-            return EXIT_SUCCESS;
-        }
-
-        // Unsupported command
-        else
-        {
-            fputs("Unsupported command, use help to display the manual\n", stderr);
-        }
-	memset(command,0,255);
-	memset(arg,0,255);
-	getcwd(cwd, sizeof(cwd));
-       	//fprintf(stdout, "%s\n", cwd);
-	printf("%s$ ", cwd);
-    }
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
